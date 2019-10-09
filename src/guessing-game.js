86161ec3169ecb13023dbf486ae63e6330f66c31
tral909 <indorm1992@mail.ru>
2019-10-09 22:00:29 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+        this.prevAssumption = undefined;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.prevAssumption = Math.round((this.max - this.min) / 2 + this.min);
+        return this.prevAssumption;
     }
 
     lower() {
-
+        this.max = this.prevAssumption;
     }
 
     greater() {
-
+        this.min = this.prevAssumption;
     }
 }
 
